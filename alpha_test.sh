91396788e39165c62ae858b2a170d3e1bb30be2d
Tarang Srivastava <tarang.sriv@berkeley.edu>
2020-12-18 00:06:36 -0500

    fixed the learning rate values

--- alpha_test.sh
+++ alpha_test.sh
@@ -1,12 +1,12 @@
-python3 rl_algos/StableBaselines.py sac fourier_4_alpha_1  --action_space=fourier --fourier_basis_size=4  --num_steps=100000  --learning_rate=0.3e-1
-python3 rl_algos/StableBaselines.py sac standard_alpha_1 --num_steps=100000 --learning_rate=0.3e-1
-python3 rl_algos/StableBaselines.py sac fourier_4_alpha_2  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=0.3e-2
-python3 rl_algos/StableBaselines.py sac standard_alpha_2 --num_steps=100000 --learning_rate=0.3e-2
-python3 rl_algos/StableBaselines.py sac fourier_4_alpha_3  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=0.3e-3
-python3 rl_algos/StableBaselines.py sac standard_alpha_3 --num_steps=100000 --learning_rate=0.3e-3
-python3 rl_algos/StableBaselines.py sac fourier_4_alpha_4  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=0.3e-4
-python3 rl_algos/StableBaselines.py sac standard_alpha_4 --num_steps=100000 --learning_rate=0.3e-4
-python3 rl_algos/StableBaselines.py sac fourier_4_alpha_5  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=0.3e-5
-python3 rl_algos/StableBaselines.py sac standard_alpha_5 --num_steps=100000 --learning_rate=0.3e-5
-python3 rl_algos/StableBaselines.py sac fourier_4_alpha_6  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=0.3e-6
-python3 rl_algos/StableBaselines.py sac standard_alpha_6 --num_steps=100000 --learning_rate=0.3e-6
+python3 rl_algos/StableBaselines.py sac fourier_4_alpha_1  --action_space=fourier --fourier_basis_size=4  --num_steps=100000  --learning_rate=3e-1
+python3 rl_algos/StableBaselines.py sac standard_alpha_1 --num_steps=100000 --learning_rate=3e-1
+python3 rl_algos/StableBaselines.py sac fourier_4_alpha_2  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=3e-2
+python3 rl_algos/StableBaselines.py sac standard_alpha_2 --num_steps=100000 --learning_rate=3e-2
+python3 rl_algos/StableBaselines.py sac fourier_4_alpha_3  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=3e-3
+python3 rl_algos/StableBaselines.py sac standard_alpha_3 --num_steps=100000 --learning_rate=3e-3
+python3 rl_algos/StableBaselines.py sac fourier_4_alpha_4  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=3e-4
+python3 rl_algos/StableBaselines.py sac standard_alpha_4 --num_steps=100000 --learning_rate=3e-4
+python3 rl_algos/StableBaselines.py sac fourier_4_alpha_5  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=3e-5
+python3 rl_algos/StableBaselines.py sac standard_alpha_5 --num_steps=100000 --learning_rate=3e-5
+python3 rl_algos/StableBaselines.py sac fourier_4_alpha_6  --action_space=fourier --fourier_basis_size=4  --num_steps=100000 --learning_rate=3e-6
+python3 rl_algos/StableBaselines.py sac standard_alpha_6 --num_steps=100000 --learning_rate=3e-6
